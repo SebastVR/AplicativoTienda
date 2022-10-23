50cac734effcf9db8c742cd0004d737764a95f4e
SebastVR <sebastianvr92@hotmail.com>
2022-10-23 11:15:00 -0500

    Se hace el listen y se prueba que el servidor sid e respesta

--- back/server.js
+++ back/server.js
@@ -1,1 +1,10 @@
-const app = require("app")
+const app = require("./app")
+
+// Setear el archivo de configuración
+const dotenv = require("dotenv")
+dotenv.config({path: 'back/config/config.env'})
+
+// LLamada del servidor
+const sever = app.listen(process.env.PORT, () => {
+  console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+})
