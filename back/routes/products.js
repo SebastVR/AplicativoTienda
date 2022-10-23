ee329d79c8a71b552273934d3c2f3826425293ca
SebastVR <sebastianvr92@hotmail.com>
2022-10-23 18:41:04 -0500

    Se hace los url del CRUD

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router();
 
-const {getProducts, newProduct} = require('../controllers/productsController');    //Traemos la respuesta json desde el controlador
+const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require('../controllers/productsController');    //Traemos la respuesta json desde el controlador
 
 router.route("/productos").get(getProducts)    //Establecemos desde que ruta queremos ver el getProducts
-router.route("/productos/nuevo").post(newProduct)    //Establemos la ruta
+router.route("/producto/nuevo").post(newProduct)    //Establecemos la ruta
+router.route("/producto/:id").get(getProductById)    //Establecemos la ruta id los : quiere decir hace parte de los parámetros del objeto
+router.route("/producto/:id").put(updateProduct)    //Ruta para actualizar 
+router.route('/producto/:id').delete(deleteProduct);    //Ruta para actualizar 
+
+
 
 module.exports = router;
