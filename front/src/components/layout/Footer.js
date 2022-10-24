5d12f2a2d6b9205d33665141ee8b068809743c3d
SebastVR <sebastianvr92@hotmail.com>
2022-10-24 09:36:36 -0500

    Se crea comonentes react para nuestro front

---
+++ front/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+export const Footer = () => {
+  return (
+    <Fragment>
+      <footer className="py-1">
+        <p className="tex-center mt-1">
+          Copicra Shop - 2022 - Misión los derechos reservados
+        </p>
+      </footer>
+    </Fragment>
+  );
+};
