8f45105ba9bd74d444bcd0c77f0977c4f206a635
SebastVR <sebastianvr92@hotmail.com>
2022-10-24 09:35:59 -0500

    Se crea archivo para compilar el front

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
+import { Footer } from './components/layout/Footer';
+import Header from './components/layout/Header';
 // import React from 'react';
 
 function App() {
   return (
     <div className="App">
-        <h1>Tienda Coprica</h1>
-        <img src="./images/estacion.png" alt="Aquí va el logo" ></img>
+      <Header />
+      <center> Contenido aqui</center>
+      <center> Contenido aqui</center>
+      <center> Contenido aqui</center>
+      <center> Contenido aqui</center>
+      <center>
+        <Footer />
+      </center>
+      {/* <h1>Tienda Coprica</h1>
+        <img src="./images/estacion.png" alt="Aquí va el logo" ></img> */}
     </div>
   );
 }
