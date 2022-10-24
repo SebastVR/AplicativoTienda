ab4f5e56c2fe83df40d9880e2b23a56b030b9f0e
SebastVR <sebastianvr92@hotmail.com>
2022-10-24 00:40:43 -0500

    Se descarga react se configura el front

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+// import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Tienda Coprica</h1>
+        <img src="./images/estacion.png" alt="Aquí va el logo" ></img>
+    </div>
+  );
+}
+
+export default App;
