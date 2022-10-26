dfaf471b7e673331fe1c6395b4c3b637f4216c48
SebastVR <sebastianvr92@hotmail.com>
2022-10-26 09:14:18 -0500

    Se agrega empoint del home

--- front/src/App.js
+++ front/src/App.js
@@ -4,15 +4,23 @@ import { Footer } from './components/layout/Footer';
 import Header from './components/layout/Header';
 import React from 'react';
 import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Home" element={<Home />} />
+          </Routes>
+        </div>
+        <Home />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
